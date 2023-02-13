96fa55d8bac317f2f9b36de40fdf6cd3eeecd9c8
IyanuoluwaOdulate <odulateiyanuoluwa2000@gmail.com>
2023-02-13 14:39:54 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function multiply (a, b) {
+    return a * b;
+}
+
+console.log(multiply(2, 5))
+
+//Variables
+//FirstName = Iyanuoluwa 
+//LastName = Odulate
+//Stutern track = Frontend Devvelopment
+//Favourite color = Lavender
+//Independence - 1960
+
+let FirstName = 'Iyanuoluwa'
+let LastName = 'Odulate'
+let Stuterntrack = 'Frontend Development'
+let Favouritecolor = 'Lavender'
+let Independence = '1960'
+
+//My name is Iyanuoluwa Odulate. I am in the Frontend Development track at Stutern. 
+//My favourite color is Lavender, and my country gained independence in 1960.
+
+console.log("My name is " + FirstName + " " + LastName + ". I am in the " + Stuterntrack + " track at Stutern.",
+"My favourite color is " + Favouritecolor + ", and my country gained independence in " + Independence + ".");
---
+++ readme.md
@@ -0,0 +1,7 @@
+  Node
+  Create web and mobile apps, game development, building interactive web pages, building web servers, developing server applications.
+  it was initially released in 1997 and lately released in June 2022.
+  console.log()
+  Frontend and backend development, Mobile development
+  Statically typed languages requires the programmer to specify what type each variable is e.g C++, C, Java while in Dynamically typed languages the type is associated with run-time values and not named variables. It doesn't require the programmer to specify types everytime e.g JavaScript, PHP, Python.
+  Null indicates the absence of a value for a variable while Undefined indicates the absence of the variable itself.
